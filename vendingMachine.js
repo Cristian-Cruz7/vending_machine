660953a810c0c32a8635b8384a5b5c95b95c207b
Cristian Cruz <camilo@CAMILO.localdomain>
2022-06-02 23:35:25 -0500

    the code was modified to show the products, their codes and stock, in table form, errors were corrected.

--- vendingMachine.js
+++ vendingMachine.js
@@ -1,68 +1,102 @@
 const prompt = require('prompt-sync')();
-let products = [
+const productsMachine = [
   ["Chocorramo", "Gansito", "Galletas"],
   ["Maizitos", "Tostacos", "Yogurt"],
   ["Papas", "Platanitos", "Ponque"]
 ];
-let stock = [
-  [5, 5, 5],
-  [5, 5, 5],
-  [5, 5, 5]
+const productStock = [
+  ["20", "5", "20"],
+  ["5", "1000", "5"],
+  ["100", "5", "5"]
 ];
 
-function showVendingMachine(arr1, arr2) {
-  let result = '';
-  let coord, stoc;
-  for (let i = 0; i < arr1.length; i++) { //0
-    for (let j = 0; j < arr1[i].length; j++) { //2
-      result = result + arr1[i][j] + '    ';
-      if (j === arr1.length - 1) {
-        coord = ('   ' + (i + 1) + '.' + (j - 1) + '       ' + (i + 1) + '.' + (j) + '       ' + (i + 1) + '.' + (j + 1))
-          //coord = ((i + 1) + '.' + (j - 1) + (i + 1) + '.' + (j) + (i + 1) + '.' + (j + 1))
-        stoc = ('    ' + arr2[i][j - 2] + '         ' + arr2[i][j - 1] + '         ' + arr2[i][j]);
-        //stoc = (arr2[i][j] + arr2[i][j + 1] + arr2[i][j + 2]);
-        result = result + `\n${coord}\n${stoc}\n`;
+function showVendingMachine(matrixProducts, matrixStock, matrixCode) {
+  const SPACES = " ";
+  const supline = "-";
+  const topLine = supline.repeat(122);
+  const spaceTable = 40; //40 size of spaces for word container in table
+  const codeProduct = 3; //3 size of the spaces used by a product code. 
+  let itemTable = '';
+  let coord, stock, spaceLeft, spaceLeft1, spaceLeft2, spaceLeft3;
+  let spaceRight, spaceRight1, spaceRight2, spaceRight3;
+  console.log(topLine);
+  //"for" block that prints the matrix of products, code and vending machine stock.
+  for (let i = 0; i < matrixProducts.length; i++) {
+    for (let j = 0; j < matrixProducts[i].length; j++) {
+      let imparLengthItem = 0;
+      let leftSpaceTable, rightSpaceTable, squareItemTable;
+      leftSpaceTable = SPACES.repeat(Math.round((spaceTable - (matrixProducts[i][j].length)) / 2));
+      //If the length of the product string is odd, the value of the variabe changes.
+      if (matrixProducts[i][j].length % 2 === 1) {
+        imparLengthItem = 1;
+      }
+      rightSpaceTable = SPACES.repeat(Math.round((spaceTable - (matrixProducts[i][j].length)) / 2) - imparLengthItem);
+      squareItemTable = `|${leftSpaceTable}${matrixProducts[i][j]}${rightSpaceTable}`;
+      //When the product stock is equal to zero, change the product name to ----.
+      if (matrixStock[i][j] <= 1) {
+        matrixProducts[i][j] = `${supline.repeat(4)}`;
+      }
+      itemTable = itemTable + `${squareItemTable}`;
+      //When the end of the row is identified, add the product code and stock data.
+      if (j === matrixProducts.length - 1) {
+        let imparsubtraction = 1;
+        //Calculate the required spaces on the left and right to organize the codes.
+        spaceLeft = SPACES.repeat(Math.round((spaceTable - (codeProduct)) / 2));
+        spaceRight = SPACES.repeat(Math.round((spaceTable - (codeProduct)) / 2) - imparsubtraction);
+        //Calculate the spaces required on the left and right to organize the stock of the product in column 1.
+        spaceLeft1 = SPACES.repeat(Math.round((spaceTable - (("Stock:" + productStock[i][j - 2]).length)) / 2))
+        spaceRight1 = SPACES.repeat((productStock[i][j - 2].length % 2 != 1) ? spaceLeft1.length : spaceLeft1.length - imparsubtraction); // We use a ternary operator to find out whether the length of the product stock is even or odd.
+        //Calculate the spaces required on the left and right to organize the stock of the product in column 2.
+        spaceLeft2 = SPACES.repeat(Math.round((spaceTable - (("Stock:" + productStock[i][j - 1]).length)) / 2))
+        spaceRight2 = SPACES.repeat((productStock[i][j - 1].length % 2 != 1) ? spaceLeft2.length : spaceLeft2.length - imparsubtraction);
+        //Calculate the spaces required on the left and right to organize the stock of the product in column 2.
+        spaceLeft3 = SPACES.repeat(Math.round((spaceTable - (("Stock:" + productStock[i][j]).length)) / 2))
+        spaceRight3 = SPACES.repeat((productStock[i][j].length % 2 != 1) ? spaceLeft3.length : spaceLeft3.length - imparsubtraction);
+        //Organize the results in the coordinates and stock variables, to add it to the itemTable variable and print the final shade.
+        coord = `|${spaceLeft}${(i + 1)}.${(j - 1)}${spaceRight}|${spaceLeft}${(i + 1)}.${(j )}${spaceRight}|${spaceLeft}${(i + 1)}.${(j + 1)}${spaceRight}|`;
+        stock = `|${spaceLeft1}Stock:${matrixStock[i][j - 2]}${spaceRight1}|${spaceLeft2}Stock:${matrixStock[i][j - 1]}${spaceRight2}|${spaceLeft3}Stock:${matrixStock[i][j]}${spaceRight3}|`;
+        itemTable = itemTable + `|\n${stock}\n${coord}\n|${topLine}\n`;
       }
     }
   }
-  console.log(result);
+  console.log(itemTable);
 }
 
-function productStock(arr3, a, b) {
-  if (a <= arr3.length & b <= arr3.length) {
-    let newStock = arr3[a][b];
-    if (newStock !== 0) {
+function showStock(matrixStock, row, column) {
+  if (row <= matrixStock.length & column <= matrixStock.length) {
+    let newStock = matrixStock[row][column];
+    //When the product stock is different from 0, it shows the quantity by subtracting, otherwise it prints a message.
+    if (newStock != 0) {
       newStock = newStock - 1;
-      arr3[a][b] = newStock;
+      matrixStock[row][column] = String(newStock);
     } else {
-      console.log('El producto ya no se encutra disponible');
+      console.log('The product is no longer available');
     }
   }
 }
 
-function productRequest(matrix, arr3) {
-  let a, b, code;
+function requestProduct(matrixProducts, matrixStock) {
+  let rowPosition, columnPosition, codeProduct;
+  let showProduct = '';
   while (1) {
-    code = prompt('Ingerese el codigo numerico del producto: ');
-    if (code[1] === '.' & code[0] <= matrix.length & code[2] <= matrix.length) {
-      a = code[0] - 1, b = code[2] - 1;
-      let prod = '';
-      prod = matrix[a][b];
-      console.log("Disfrute su producto: " + prod);
-      productStock(arr3, a, b);
+    codeProduct = prompt('Enter the product code number: ');
+    //If it is a valid code, it prints on console the corresponding product, otherwise it prints an invalid code message.
+    if (codeProduct[1] === '.' & codeProduct[0] <= matrixProducts.length & codeProduct[2] <= matrixProducts.length) {
+      rowPosition = codeProduct[0] - 1;
+      columnPosition = codeProduct[2] - 1;
+      showProduct = matrixProducts[rowPosition][columnPosition];
+      console.log("Enjoy your product: " + showProduct);
+      showStock(matrixStock, rowPosition, columnPosition);
     } else {
-      console.log('Codigo incorrecto, intente nuevamente.');
+      console.log('Wrong code, please try again.');
     }
-    console.log();
-    showVendingMachine(products, stock);
-
+    showVendingMachine(productsMachine, productStock);
   }
-
 }
 
 function main() {
-  showVendingMachine(products, stock);
-  productRequest(products, stock);
+  showVendingMachine(productsMachine, productStock);
+  requestProduct(productsMachine, productStock);
 }
 
 main();
